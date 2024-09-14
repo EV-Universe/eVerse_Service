3d51dae0b6fd8d8112304e564fe755feca29c0d8
Vishal-781 <vishalshrivastava914419@gmail.com>
2024-09-14 17:01:34 +0530

    JWT Authentication in progress

---
+++ src/main/java/com/vishal/eVerse/config/JwtAuthenticationFilter.java
@@ -0,0 +1,47 @@
+package com.vishal.eVerse.config;
+
+import com.vishal.eVerse.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+//    private final UserService userService;
+    private  final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+          @NotNull HttpServletRequest request,
+          @NotNull HttpServletResponse response,
+          @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        String jwt = null;
+        String userEmail = null;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractName(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
+
+    }
+}
---
+++ src/main/java/com/vishal/eVerse/models/Role.java
@@ -0,0 +1,5 @@
+package com.vishal.eVerse.models;
+
+public enum Role {
+    USER, ADMIN
+}
