4df17bad4ae2910240b1751e58bc71db7c009025
Vishal-781 <vishalshrivastava914419@gmail.com>
2024-09-24 16:16:56 +0530

    JWT Filter done

--- src/main/java/com/vishal/eVerse/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vishal.eVerse.repository;
-
-import com.vishal.eVerse.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String Email);
-}
